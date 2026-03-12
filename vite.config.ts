73d1e17db285148e3d3a0f8a24df10cd5398460f
Hiren Parekh <parekhhiren@outlook.com>
2026-03-11 20:38:06 -0400

    build: update config for GitHub pages and add build output

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/piano-tiles/',
+  build: {
+    outDir: 'docs',
+  },
   plugins: [react()],
   server: {
     host: true,
